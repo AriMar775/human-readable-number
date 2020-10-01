9bd40f74656790baec1b1996a6ee767dc27ebbd4
Arina Marchenko <arinamar7705@gmail.com>
2020-10-01 15:20:00 +0300

    init: start task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function toReadable (number) {
-  
+  let result = "";
+  let numbers = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
+  let teen = ["eleven", "twelve", "teen"];
+  let tens = ["ten","twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"]
+  let bigNumbers = ["hundred", "thousand", "million", "billion"]
+
+  if (number.length > 9) result += "!!!!!!" + bigNumbers[3]
+  return number;
+
+  /*вместо массивов объект и сравнивать поля?*/
 }
